de4e744ced0adb0825083531aa712a245d435977
azkamuhamco <reprokbas@gmail.com>
2023-03-05 18:21:46 +0700

    masuk midlw 02

--- pkg/middleware/fiber.go
+++ pkg/middleware/fiber.go
@@ -3,7 +3,6 @@ package middleware
 import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
-	"github.com/gofiber/fiber/v2/middleware/logger"
 )
 
 // FiberMiddleware provide Fiber's built-in middlewares.
@@ -12,7 +11,5 @@ func FiberMiddleware(a *fiber.App) {
 	a.Use(
 		// Add CORS to each route.
 		cors.New(),
-		// Add simple logger.
-		logger.New(),
 	)
 }
